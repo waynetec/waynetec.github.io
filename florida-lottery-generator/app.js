3a5bb40484ada44f90221f105e708c244111ddd1
waynetec <46908942+waynetec@users.noreply.github.com>
2022-07-15 19:32:16 -0400

    Add files via upload

---
+++ florida-lottery-generator/app.js
@@ -0,0 +1,54 @@
+
+var titleDiv = document.getElementById("title-div");
+var containerDiv = document.getElementById("container-div");
+var disclaimerDiv = document.getElementById("disclaimer-div");
+var refreshDiv = document.getElementById("refresh-div");
+var refreshButton = document.createElement("button");
+
+refreshButton.textContent = "Generate";
+refreshButton.classList.add("refresh-button");
+refreshDiv.append(refreshButton);
+
+var clickRefresh = refreshButton.addEventListener('click', function(){document.location.reload()});
+
+document.title = "Florida Lottery Numbers Generator";
+titleDiv.textContent = "Florida Lottery Numbers Generator";
+disclaimerDiv.textContent ="** This site is not affiliated with the Florida Lottery. This generator is just for fun and you assume all risk involved in using it. Using this generator to pick your numbers does not increase your odds of winning **";
+
+
+
+var regularNumbers = 6;
+var highestRegNumber = 52;
+
+var numbersDrawn = [];
+
+for(var j = 0; j < regularNumbers; j++){
+
+    var randomNumber = Math.round((Math.random() * highestRegNumber + 1),2);
+    numbersDrawn.push(randomNumber);
+    numbersDrawn.sort(sort);
+}
+
+numbersDrawn.sort(sort);
+
+//Check for duplicates in array
+for(var k=0; k < numbersDrawn.length; k++){
+    if(numbersDrawn[k] == numbersDrawn[k-1] && k > 0){
+        randomNumber = Math.round((Math.random() * highestRegNumber + 1),2);
+        numbersDrawn[k] = randomNumber;
+        numbersDrawn.sort(sort);
+    }
+}
+
+for(var i = 0; i < regularNumbers; i++){
+    var lotteryBall = document.createElement("div");
+    lotteryBall.classList.add("lottery-ball");
+
+
+    lotteryBall.textContent = numbersDrawn[i];
+
+    containerDiv.append(lotteryBall);
+}
+
+//Sort function
+function sort(a, b){return a-b;}
