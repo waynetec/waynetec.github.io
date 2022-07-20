aaf85c463eadfee9906261f3af791e18fec9ba74
waynetec <46908942+waynetec@users.noreply.github.com>
2022-07-20 16:26:52 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ navDiv.append(navUL);
 var myLinks = [
     [
         "Single Dice Tosser",
-        "https://waynetec.github.io/dice-on-canvas/diceOnCanvas.html"
+        "https://waynetec.github.io/dice-on-canvas/index.html"
     ],
     [
         "Bible Memory Verses",
